0182a84d875234a0011b73c23e03f98a53adc2ce
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-10-15 11:13:09 +0600

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-let btnDropdown = document.querySelector(".btn-dropdown");
 const btnIcon = document.getElementById("btn_icon");
 
 btnIcon.addEventListener("click", function () {
-  console.log("hello");
   const btnDropdown = document.querySelector(".btn-dropdown");
-  btnDropdown.classList.add("btn-dropdown-visibility");
+  btnDropdown.classList.add("visibility-check");
 });
 
-// document.getElementById("btn_icon").onclick = () => {
-//   login.classList.toggle("active");
-// };
+const logsBtn = document.querySelector(".logs-icon");
+
+logsBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const branchMenu = document.querySelector(".branch-menu");
+  branchMenu.classList.add("branch-menu-display");
+  logsBtn.style.transform = "scale(-1, -1)";
+});
